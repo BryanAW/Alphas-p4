bdc74b9da95d125add5b62835f0d53b5d2bdaa0d
bryanwitherspoon <bryanwitherspoon1066@outlook.com>
2024-02-15 17:36:27 -0500

    Completed problem 3 and implemented into web apps

---
+++ components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+/* Ensure this is added to every web app */
+function Header() {
+    return (
+        <div className="header">
+            <img src="https://giffiles.alphacoders.com/576/57604.gif" alt="Alpha" />
+            <h1>Alphas Project 4</h1>
+        </div>
+    );
+}
+
+export default Header;
--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/main.css';
 
 import Example from './components/example/Example';
+import Header from './components/header/Header';
 
+/* Render the Header and Example components */
 ReactDOM.render(
-  <Example />,
+    <>
+        <Header />
+        <Example />
+    </>,
   document.getElementById('reactapp'),
 );
