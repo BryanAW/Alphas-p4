d7526598a1cd729063e7c87316740d86a916dbdc
bryanwitherspoon <bryanwitherspoon1066@outlook.com>
2024-02-15 18:10:52 -0500

    Completed problem 4 - dynamic switching

---
+++ p4.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import necessary components
+import Example from './components/example/Example';
+import States from './components/states/States';
+import Header from './components/header/Header';
+
+// Define the Main component
+function Main() {
+    // State is used to keep track of the current view
+    const [curView, setCurView] = React.useState('example');
+
+    // Function to switch the views
+    const switchView = () => {
+        setCurView(curView === 'Example' ? 'States' : 'Example');
+    };
+
+    const buttonText = curView === 'Example' ? 'Switch to States' : 'Switch to Example';
+
+    // Rendering the Header and the current view
+    return (
+        <>
+            <Header />
+            <button onClick={switchView}>{buttonText}</button>
+            {curView === 'Example' ? <Example /> : <States />}
+        </>
+    );
+}
+
+// Render the Main component into our DOM
+ReactDOM.render(
+    <Main />,
+    document.getElementById('reactapp'),
+);
