68fb05062eba26fe2d4789436b4dedd29f1eb07c
Jameson Woodard <156945354+JWoodard22@users.noreply.github.com>
2024-02-16 11:15:27 -0500

    Create p5.jsx
    added the react import

---
+++ p5.jsx
@@ -0,0 +1,37 @@
+import { HashRouter, Route, Link } from "react-router-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import necessary components
+import Example from './components/example/Example';
+import States from './components/states/States';
+import Header from './components/header/Header';
+import './styles/main.css';
+
+// Define the Main component
+function Main() {
+    // State is used to keep track of the current view
+    const [curView, setCurView] = React.useState('Example');
+
+    // Function to switch the views
+    const switchView = () => {
+        setCurView(curView === 'Example' ? 'States' : 'Example');
+    };
+
+    const buttonText = curView === 'Example' ? 'Switch to States' : 'Switch to Example';
+
+    // Rendering the Header and the current view
+    return (
+        <>
+            <Header />
+            <button onClick={switchView}>{buttonText}</button>
+            {curView === 'Example' ? <Example /> : <States />}
+        </>
+    );
+}
+
+// Render the Main component into our DOM
+ReactDOM.render(
+    <Main />,
+    document.getElementById('reactapp'),
+);
